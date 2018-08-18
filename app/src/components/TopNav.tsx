d646a58fe48e6cdc8fefe5f88a16740640b95567
Mike Yoon <github873@yaki.org>
2018-08-17 21:48:36 -0700

    Styling stuff

--- app/src/components/TopNav.tsx
+++ app/src/components/TopNav.tsx
@@ -37,51 +37,44 @@ export class TopNav extends React.Component<BaseProps> {
         const searchBtnClasses = classnames('btn', 'btn-primary', { disabled: this.props.store.searchStore.searching });
         const isLoggedIn = this.props.store.authStore.isLoggedIn;
 
-        return <nav className="navbar navbar-default">
-            <div className="container">
-                <div className="navbar-header">
-                    <button className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
-                        <span className="sr-only">Toggle Navigation</span>
-                        <span className="icon-bar" key="1"/>
-                        <span className="icon-bar" key="2"/>
-                        <span className="icon-bar" key="3"/>
-                    </button>
-                    <a className="navbar-brand" href="/journal">My Daily Stuff</a>
-                </div>
+        return <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="/journal">My Daily Stuff</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
 
-                <div className="collapse navbar-collapse navbar-ex1-collapse" id="navbar">
-                    {this.props.store.authStore.isLoggedIn ?
-                    <form className="navbar-form navbar-left" role="search" onSubmit={this.handleSearch}>
-                        <div className="input-group">
-                            <input className="form-control" type="search" placeholder="Search"
-                                   onChange={e => this.updateQueryText(e.target.value)}
-                                   onKeyDown={this.handleSearchKeyDown}/>
-                            <div className="input-group-btn">
-                                <button type="submit"
-                                        className={searchBtnClasses}>
-                                    <i className="glyphicon glyphicon-search"/>
-                                </button>
-                            </div>
+            <div className="collapse navbar-collapse justify-content-end" id="navbar">
+                {this.props.store.authStore.isLoggedIn ?
+                <form className="navbar-form navbar-left" role="search" onSubmit={this.handleSearch}>
+                    <div className="input-group">
+                        <input className="form-control" type="search" placeholder="Search"
+                                onChange={e => this.updateQueryText(e.target.value)}
+                                onKeyDown={this.handleSearchKeyDown}/>
+                        <div className="input-group-btn">
+                            <button type="submit"
+                                    className={searchBtnClasses}>
+                                <i className="glyphicon glyphicon-search"/>
+                            </button>
                         </div>
-                    </form>: null
-                        }
+                    </div>
+                </form> 
+                : null }
 
-                    <ul className="nav navbar-nav navbar-right">
-                        {isLoggedIn ?
-                        <li className="dropdown">
-                            <a className="dropdown-toggle" data-toggle="dropdown" role="button">
-                                {this.props.store.authStore.email}
-                                <span className="caret"/>
-                            </a>
-                            <ul className="dropdown-menu" role="menu">
-                                <li key="1"><a href="/profile">My Profile</a></li>
-                                <li key="2"><a href="#" onClick={this.handleLogout}>Logout</a></li>
-                            </ul>
-                        </li> :
-                        <li key="Login"><a href="/login">Login</a></li>}
-                        {isLoggedIn ? null : <li key="Register"><a href="/register">Register</a></li>}
-                    </ul>
-                </div>
+                <ul className="nav navbar-nav navbar-right">
+                    {isLoggedIn ?
+                    <li className="dropdown">
+                        <a className="dropdown-toggle" data-toggle="dropdown" role="button">
+                            {this.props.store.authStore.email}
+                            <span className="caret"/>
+                        </a>
+                        <ul className="dropdown-menu" role="menu">
+                            <li key="1"><a href="/profile">My Profile</a></li>
+                            <li key="2"><a href="#" onClick={this.handleLogout}>Logout</a></li>
+                        </ul>
+                    </li> :
+                    <li key="Login"><a href="/login">Login</a></li>}
+                    {isLoggedIn ? null : <li key="Register"><a href="/register">Register</a></li>}
+                </ul>
             </div>
         </nav>
     }
