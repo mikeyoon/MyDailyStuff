21874f717881127c2ce06214aafdab40f11ec333
Mike Yoon <github873@yaki.org>
2021-08-09 22:42:16 -0700

    WIP

---
+++ app/src/index.ts
@@ -0,0 +1,16 @@
+import { AboutComponent } from './components/about/about.component.js';
+
+customElements.define('mds-about', AboutComponent);
+
+// class MyStore extends EventTarget {
+//   constructor() {
+//     super();
+//   }
+
+//   init() {
+//     this.addEventListener('notify', );
+//   }
+// }
+
+// customElements.define('app', )
+
